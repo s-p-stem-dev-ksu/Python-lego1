5f65fb1a32b008f74681c28aeca7429227e5db5b
s-p-stem-dev-ksu <48621078+s-p-stem-dev-ksu@users.noreply.github.com>
2019-03-23 13:42:05 +0200

    Create traffic-lights.py

---
+++ traffic-lights.py
@@ -0,0 +1,13 @@
+from ev3dev2.led import Leds
+import time
+lights = Leds()
+while True:
+  lights.set_color("LEFT", "RED")
+  lights.set_color("RIGHT", "RED")
+  time.sleep(1)
+  ights.set_color("LEFT", "AMBER")
+  lights.set_color("RIGHT", "AMBER")
+  time.sleep(1)
+  ights.set_color("LEFT", "GREEN")
+  lights.set_color("RIGHT", "GREEN")
+  time.sleep(1)
